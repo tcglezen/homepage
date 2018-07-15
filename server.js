b3162f5042605dd4fb7094f21436cf2f98def1c6
Thomas <tcglezen@berkeley.edu>
2018-07-14 21:12:25 -0700

    Moved all styling to tcglezenCSS, added some pictures. There are also some new files.

--- server.js
+++ server.js
@@ -6,23 +6,19 @@ const app = express();
 const port = process.env.PORT || 9001;
 const fs = require('fs'); 
 
-
 app.use(express.static(__dirname));
 
 //express.static('C:/homepage');
 
-
 //https://nodejs.org/dist/latest-v8.x/docs/api/http.html#http_http_createserver_requestlistener
+//Thisis the standard page. 
 app.get('/', (req, res) => {
 	res.writeHead(200, {'Content-Type': 'text/plain'});
-	//res.write('This is the first line\n');
 	res.end();
 	}
 );
 
-//This will be the 404 error 
-//Learn how to redirect this to a spare page 
-//
+//404 page. 
 app.get('/*', (req, res) => {
 	
 	fs.readFile('404page.html', function (err, data) { 
@@ -30,14 +26,13 @@ app.get('/*', (req, res) => {
 		res.write(data); 
 		return res.end(); 
 	    })
-		
-		
-		
 	//res.writeHead(404, {'Content-Type': 'text/html'});
 	//res.sendFile('./404page.html');
     });
+	
 
 app.listen(port, () => {
 	console.log(`listening on port ${port}`)
 })
+
 
