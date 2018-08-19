ed4e8eaa6c6d7f01cf2415527c441d54db477163
Thomas <tcglezen@berkeley.edu>
2018-08-19 14:29:52 -0700

    buggy message section added

--- sideProjects/messageBoard/firebaseMessage.js
+++ sideProjects/messageBoard/firebaseMessage.js
@@ -1,5 +1,4 @@
-const firebase = require('firebase');
-
+//const firebase = require('firebase');
   // Initialize Firebase
 const config = {
   apiKey: "AIzaSyDHsjQhhZApTOlN_0tC8-9D0AV7RfGZeu0",
@@ -12,54 +11,116 @@ const config = {
 
 firebase.initializeApp(config);
 
-//Other things that firebase can do is
-/*
-firebase.auth() - Authentication
-firebase.storage() - Cloud Storage
-fireabse.database() - realtime database
-firebase.messaging() - cloud messaging
-firebase.functions() - cloud functions
-*/
+var messageKeys = [];
+var tempMessage = {};
+var messageList = [];
+var messageCount = 0;
+var displayCount = 0;
 
-/*STEPS
-0.) Display all of the information
-1.) User inputs a bunch of stuff inside of the text box
-2.) User presses a nearby button.
-
-3.) The information inside of the textbox will be sent to firebase
-4.) The text inside the textbox will be cleared
-
-5.) Don't forget to display all of (new) the information
-*/
-
-/*What does each message look like?
-Maybe make 2 fields
-1.) "Name" (yes, they can fake it)
-2.) The message itself
-*/
-
-function writeName(enteredName) {
-  firebase.database().ref('message/name').push({
-    name: enteredName
-  });
-}
-
-function writeText(enteredText) {
-  firebase.database().ref('message/text').push({
-    text: enteredText
-  });
-}
-
-//Maybe this should have an index for which message we would like to read
-function readName() {
-
-}
-
-function readText() {
-
-}
-
-//Maybe delete all of the messages?
 function clearMessages() {
   firebase.database().ref('message').set(null);
+  //clearDisplay();
+  messageKeys = [];
+  messageCount = 0;
+}
+
+function superFunction() {
+  clearDisplay();
+  writeFunction();
+  startLoad();
+  //readFunction(messageCount);
+  //displayAll(messageList);
+}
+
+/*
+The purpose of the write functions is to
+1.) Obtain the values from the text fields
+2.) Obtain the keys needed for database (and add them to list)
+*/
+
+
+function writeFunction() {
+  var enteredName = document.getElementById("messageNameField")
+  var enteredText = document.getElementById("messageTextField")
+
+  var tempKey = firebase.database().ref('message').push({
+    name: enteredName.value,
+    text: enteredText.value
+  }).key;
+  messageKeys.push(tempKey);
+  document.getElementById('messageNameField').value = '';
+  document.getElementById('messageTextField').value = '';
+  //messageCount ++;
+}
+
+function clearDisplay() {
+  for(i = 0; i < displayCount; i++) {
+    document.getElementById('div' + String(i)).remove();
+  }
+}
+
+//Pushes values via key list into personal list
+function readFunction(index) {
+  tempLoc = firebase.database().ref('message/'+messageKeys[index]);
+  tempLoc.once('value', function (snapshot) {
+    tempMessage = snapshot.val();
+    messageList.push(tempMessage);
+  });
+}
+
+function singleDisplay(divNum, messy) {
+  var d = document.createElement("div");
+  d.id = "div" + String(divNum);
+
+  var pName = document.createElement('P');
+  pName.id = 'name';
+  var pText = document.createElement('P');
+  pText.id = 'text';
+  var hrTag = document.createElement('hr');
+
+  pNameInner = document.createTextNode(messy.name);
+  pTextInner = document.createTextNode(messy.text);
+
+  pName.appendChild(pNameInner);
+  pText.appendChild(pTextInner);
+
+  d.appendChild(pName);
+  d.appendChild(pText);
+  d.appendChild(hrTag);
+
+  document.getElementById('messageList').appendChild(d);
+}
+
+function displayAll(mList){
+  for (i = 0; i < mList.length; i++) {
+    singleDisplay(i, mList[i]);
+  }
+  displayCount = mList.length;
+}
+
+//.key() should get the key value, similar to .key
+function startLoad() {
+
+  messageKeys = [];
+  messageList = [];
+  messageCount = 0;
+
+  var mainRef = firebase.database().ref('message')
+  mainRef.once('value', (snapshot) => {
+    snapshot.forEach((item) => {
+      messageKeys.push(item.key);
+    })
+    messageKeys.forEach((key, i) => {
+      readFunction(i);
+    })
+  })
+/*  }).then(() => {
+      messageCount = messageKeys.length;
+      for (i = 0; i < messageCount; i++) {
+        readFunction(i);
+      }*/
+    .then(() => {
+      console.log(messageList);
+      displayAll(messageList);
+    });
 }
