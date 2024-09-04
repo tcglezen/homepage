94552f4bd132d359e908381a33374626e5962da7
Thomas Glezen <tcglezen@gmail.com>
2024-09-04 10:53:29 -0700

    Removed excess code

--- Projects/funjs/shady_copy.js
+++
@@ -1,8 +0,0 @@
-
-document.getElementById('copy_me').addEventListener( 
-'copy', function(e) { 
-    e.clicpboardData.setData('text/plain', 
-        'HACKERMAN' 
-    ); 
-    e.preventDefault(); 
-}); 
